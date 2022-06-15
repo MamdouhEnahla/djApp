c8da1a0dc9cd03ebda04d5fd7171cb251d8855a0
codingPro <geek@codingPro.com>
2022-06-15 15:54:44 +0200

    bootstrap and jinja staff

--- first_porject/boards/urls.py
+++ first_porject/boards/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='index')
+    path('', views.home, name='index'),
+    path('about/', views.about, name='about'),
+    path('boards/<int:board_id>', views.getTopics, name="getTopics"),
+    path('boards/<int:board_id>/new', views.addTopic, name="addTopic"),
 ]
--- first_porject/boards/views.py
+++ first_porject/boards/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Board
-# Create your views here.
+# homepage.
 
 def home(request):
     boards = Board.objects.all()
@@ -9,3 +10,23 @@ def home(request):
         'boards':boards
     }
     return render(request, 'home.html', context)
+
+#view current topics
+def getTopics(request, board_id):
+    board = get_object_or_404(Board, pk=board_id)
+    context ={
+        'board': board
+    }
+    return render(request, 'topics.html', context)
+
+#add newTopic
+def addTopic(request, board_id):
+    board = get_object_or_404(Board, pk=board_id)
+    context={
+        'board': board
+    }
+    return render(request, 'addtopic.html', context)
+
+#about page
+def about(request):
+    return HttpResponse("<h1>About page</h1>")
