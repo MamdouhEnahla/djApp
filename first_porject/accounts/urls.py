ca0bb62fef0786ee872856d051c0285910fc19b2
codingPro <geek@codingPro.com>
2022-07-24 22:39:30 +0200

    some editing to file names

--- first_porject/accounts/urls.py
+++ first_porject/accounts/urls.py
@@ -6,8 +6,8 @@ urlpatterns =[
     path('signup/', views.signup, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name ='accounts/login.html'), name='login'),
-    path('update_password/', auth_views.PasswordChangeView.as_view(template_name ='accounts/update_password.html'), name='update_password'),
-    path('updated_password/', auth_views.PasswordChangeDoneView.as_view(template_name ='accounts/updated_password.html'), name='updated_password'),
+    path('change_password/', auth_views.PasswordChangeView.as_view(template_name ='accounts/change_password.html'), name='change_password'),
+    path('updated_password/', auth_views.PasswordChangeDoneView.as_view(template_name ='accounts/password_change_done.html'), name='password_change_done'),
 
 
 
