f8b49d041875586e785c49824e2ff61f951e65d8
codingPro <geek@codingPro.com>
2022-08-05 08:01:57 +0200

    using class-based-view

--- first_porject/boards/views.py
+++ first_porject/boards/views.py
@@ -1,10 +1,13 @@
+
+from .models import *
 from django.db.models import Count
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .forms import addNewTopic, PostForm
-from .models import *
+from django.utils import timezone
+from django.views.generic import UpdateView
 
 # homepage.
 def home(request):
@@ -83,6 +86,21 @@ def reply_topic(request, board_id,topic_id):
         'form': form, 
     }
     return render(request,'reply_topic.html', context)
+
+#class-based-view
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ("message",)
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_id'
+    context_object_name = 'post'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_dt = timezone.now()
+        post.save()
+        return redirect('topic_posts', board_id= post.topic.board.pk, topic_id= post.topic.pk )
 
 #about page
 def about(request):
