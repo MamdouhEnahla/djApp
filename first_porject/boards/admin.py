1c246df6a90f8ceb4062805899f249d113e50648
codingPro <geek@codingPro.com>
2022-08-13 17:32:06 +0200

    modificationss

--- first_porject/boards/admin.py
+++ first_porject/boards/admin.py
@@ -1,4 +1,23 @@
+from inspect import classify_class_attrs
 from django.contrib import admin
-from .models import Board
+from .models import *
 # Register your models here.
-admin.site.register(Board)
+admin.site.site_header ='Boards Control Panel'
+admin.site.site_title = 'Boards Administration'
+
+class InlineTopic(admin.TabularInline):
+    model = Topic
+    extra = 1
+
+class BoardAdmin(admin.ModelAdmin):
+    inlines = [InlineTopic]
+    
+
+class TopicAdmin(admin.ModelAdmin):
+    fields = ('subject', 'board', 'views')
+    list_display = ('subject', 'board', 'views')
+
+
+admin.site.register(Board, BoardAdmin)
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Post)
--- first_porject/boards/apps.py
+++ first_porject/boards/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+# from suit.apps import DjangoSuitConfig
 
-
+# class SuitConfig(DjangoSuitConfig):
+#     layout = 'horizontal'
 class BoardsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'boards'
